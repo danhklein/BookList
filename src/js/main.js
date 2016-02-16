2a595da53e534e49ac97b43a66f3c246a493dac3
Daniel Klein <danhirschklein@gmail.com>
2016-02-15 19:20:26 -0700

    Transitioned to Michael's solution

--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,63 @@
-// add scripts
+// *** constructors *** //
 
-  console.log('sanity check!');
-
-var Book = function (attributes) {
-this.title = attributes.title;
-this.genre = attributes.genre;
-this.author = attributes.author
-this.read =false;
-this.startDate ="";
-this.endDate="";
-  this.startDate = new Date();
-  this.endDate = function() {
-    if (this.read = true) {
-      return new Date() }
-    else return "Unfinished";
-  };
+var Book = function(attributes) {
+  this.title = attributes.title;
+  this.genre = attributes.genre;
+  this.author = attributes.author;
+  this.read = false;
+  this.startDate = '';
+  this.endDate = '';
 };
 
-var BookList = function () {
+var BookList = function() {
   this.numRead = 0;
   this.numNotRead = 0;
-  this.nextBook = "";
-  this.currentBook = "";
-  this.lastBook = "";
+  this.nextBook = '';
+  this.currentBook = '';
+  this.lastBook = '';
   this.bookArr = [];
 };
 
+
+// *** methods **** //
+
 BookList.prototype.addBook = function(book) {
   this.bookArr.push(book);
-  if (this.currentBook ==='') {
+  this.numNotRead += 1;
+  if (this.currentBook === '') {
     this.currentBook = book;
-  } else if (this.nextBook=== '') {
+  } else if (this.nextBook === '') {
     this.nextBook = book;
   }
 };
 
-Book.prototype.finishCurrentBook = function() {
+BookList.prototype.startCurrentBook = function() {
+  this.currentBook.startDate = new Date(Date.now());
+};
+
+BookList.prototype.finishCurrentBook = function() {
   this.numRead += 1;
   this.numNotRead -= 1;
   this.currentBook.read = true;
   this.currentBook.endDate = new Date(Date.now());
   this.lastBook = this.currentBook;
   this.currentBook = this.nextBook;
-  var index = this.bookArr.indexOf(currentBook);
-  this.nextBook = this.bookArr[index+1] || '';
+  var index = this.bookArr.indexOf(this.currentBook);
+  this.nextBook = this.bookArr[index + 1] || '';
 };
 
-BookList.prototype.startCurrentBook = function(first_argument) {
-  this.currentBook.startDate = newDate(Date.now());
+BookList.prototype.renderToDom = function() {
+  for (var i = 0; i < this.bookArr.length; i++) {
+    $('#books').append('<tr><td>' + this.bookArr[i].title +
+      '</td><td>' + this.bookArr[i].genre +
+      '</td><td>'+ this.bookArr[i].author +
+      '</td><td>' + this.bookArr[i].read + '</td></tr>');
+  }
+  if(myBooks.numNotRead > 0) {
+    $('#books').append('<br><button id="read" class="btn btn-primary btn-md">Read Next Book!</button>');
+  }
 };
 
-BookList.prototype.renderToDom = function(){
-    for (var i = 0; i < this.bookArr.length; i++) {
-        $("#books").append("<li>" +
-            this.bookArr[i].title +
-            "&nbsp; &nbsp;"+
-            this.bookArr[i].read +
-            "</li>");
-    }
-    $("#books").append("<br><button id='read' class='btn btn-success' btn-xs'>Read Me</button>");
-};
 var myBookList = new BookList();
 var book1 = new Book({
   title: 'Labyrinths',
